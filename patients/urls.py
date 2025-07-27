4956d59d271c7cfde873716d7ff6da6af9f63f04
Husseinlearn <Hussein1Learn@gmail.com>
2025-07-27 17:30:48 +0300

    تصحيح الروابط

--- patients/urls.py
+++ patients/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('patient/', views.PatientListCreateAPIView.as_view(), name='patient-list-create'),
+    path('', views.PatientListCreateAPIView.as_view(), name='patient-list-create'),
     path('patient/<uuid:pk>/', views.PatientRetrieveUpdateDestroyAPIView.as_view(), name='patient-retrieve-update-destroy'),
 ]

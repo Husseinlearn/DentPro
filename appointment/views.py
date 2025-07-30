8e278877c2dc9cfb44a1fe094719d5a859b159c7
Husseinlearn <Hussein1Learn@gmail.com>
2025-07-31 01:48:20 +0300

    تم انشاء الفلترة

--- appointment/views.py
+++ appointment/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django_filters.rest_framework import DjangoFilterBackend
 # Create your views here.
 
 from rest_framework import generics, permissions
@@ -15,6 +15,8 @@ class AppointmentCreateAPIView(generics.CreateAPIView):
 class AppointmentListAPIView(generics.ListAPIView):
     queryset = Appointment.objects.all().order_by('-date', '-time')
     serializer_class = AppointmentSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['doctor', 'patient', 'date', 'status']
     # permission_classes = [permissions.IsAuthenticated]
 
 class AppointmentUpdateAPIView(generics.RetrieveUpdateAPIView):
